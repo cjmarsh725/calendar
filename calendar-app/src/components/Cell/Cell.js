585f65d9310d2aba8cf39c18e85402d11c8bf105
Chris <cjmarsh725@gmail.com>
2018-12-20 18:37:26 -0800

    initialized component structure

---
+++ calendar-app/src/components/Cell/Cell.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Cell.css';
+
+class Cell extends Component {
+  render() {
+    return (
+      <div>Cell</div>
+    );
+  }
+}
+
+export default Cell;
---
+++ calendar-app/src/components/Row/Row.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Row.css';
+
+class Row extends Component {
+  render() {
+    return (
+      <div>Row</div>
+    );
+  }
+}
+
+export default Row;
